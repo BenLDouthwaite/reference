d26a06ae81337347356092a474ce4f1950d311a8
Ben L Douthwaite <Ben.L.Douthwaite@gmail.com>
2019-07-15 22:34:06 +0900

    Further go tour notes

--- go/tour.go
+++ go/tour.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"math"
 	"math/rand"
+	"time"
 )
 
 var global bool
+
+const Pi = 3.14159
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
@@ -25,8 +27,49 @@ func main() {
 	vars()
 	types()
 	typeconversions()
+	//bitshifting()
+
+	looping()
+	branching()
 }
 
+func branching() {
+	x := 7
+	if x < 10 {
+		fmt.Println("X is less than 10, val = ", x)
+	}
+
+	// if can pre started with a short statment, limited to its scope
+	if pow := x * x; pow < 100 {
+		fmt.Println("Pow < 100. val = ", pow)
+	}
+	// Could now use 'pow' here
+
+	if half := x / 2; half > 1 {
+		fmt.Println("Half greater than 1. val = ", half)
+	} else {
+		fmt.Println("Half less than 1. val = ", half)
+	}
+	// half cannot be used here
+}
+
+func looping() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+
+	counter := 1
+	for counter < 100 { // Init and post statements optional
+		counter += counter
+	}
+
+	/** The following would be an infinit loop
+	for {
+	}
+	**/
+	fmt.Println("SUM = ", sum, ".Counter = ", counter)
+}
 
 func add(x int, y int) int {
 	return x + y
@@ -59,3 +102,11 @@ func typeconversions() {
 	u := uint(f)
 	fmt.Println("Formatted u ", u)
 }
+
+const (
+	// Create a huge number by shifting a 1 bit left 100 places.
+	// In other words, the binary number that is 1 followed by 100 zeroes.
+	Big = 1 << 100
+	// Shift it right again 99 places, so we end up with 1<<1, or 2.
+	Small = Big >> 99
+)
