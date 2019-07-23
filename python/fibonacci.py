78a092cd4a80949b8ae9ae9dfe7284c6b34b3c0f
Ben L Douthwaite <Ben.L.Douthwaite@gmail.com>
2019-07-23 19:11:16 +0900

    Fib to functions and add primality test

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,11 @@
-a, b = 0, 1
-while a < 10:
-    print(a)
-    a, b = b, a + b
+
+def fib(n):
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a + b
+    return result
+
+fib100 = fib(100)
+print(fib100)
--- python/syntax.py
+++ python/syntax.py
@@ -1,6 +1,7 @@
 
 # If statements
-x = int(input("Please enter an integer: "))
+# x = int(input("Please enter an integer: "))
+x = 12
 if x < 0:
     x = 0
     print('Negative changed to zero')
@@ -19,3 +20,21 @@ for w in words:
 # Range function
 for i in range(5):
     print(i)
+
+# in keyword
+if 'ben' in ('ben', 'john', 'adam'):
+    print('yup, ben is there')
+
+# keyword arguments
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.")
+    print("-- Lovely plumage, the", type)
+    print("-- It's", state, "!")
+
+parrot(1000)                                          # 1 positional argument
+parrot(voltage=1000)                                  # 1 keyword argument
+parrot(voltage=1000000, action='VOOOOOM')             # 2 keyword arguments
+parrot(action='VOOOOOM', voltage=1000000)             # 2 keyword arguments
+parrot('a million', 'bereft of life', 'jump')         # 3 positional arguments
+parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword
