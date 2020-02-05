3a247b92db623e394ed5572b2dce6344b37ee8d6
Ben Douthwaite <Ben.L.Douthwaite@gmail.com>
2020-02-05 11:53:58 +0900

    Adding repeated string challenge and test cases

---
+++ java/coding-challenges/src/main/java/RepeatedString.java
@@ -0,0 +1,19 @@
+public class RepeatedString {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+        if (s == null || s.length() == 0) {
+            throw new IllegalArgumentException("s cannot be empty");
+        }
+        long factor = n / s.length();
+        long factorResult = getCharacterCount(s, 'a') * factor;
+
+        long rem = n % s.length();
+        long remResult = getCharacterCount(s.substring(0, (int)rem), 'a');
+        return factorResult + remResult;
+    }
+
+    private static long getCharacterCount(String s, char characterToCount) {
+        return s.chars().filter(ch -> ch == characterToCount).count();
+    }
+}
---
+++ java/coding-challenges/src/test/java/RepeatedStringTest.java
@@ -0,0 +1,92 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class RepeatedStringTest {
+
+    @Test
+    public void repeatedString_problemDefinition_0() {
+
+        String repeatingString = "aba";
+        long characterLimit = 10L;
+
+        long expectedCharacterCount  = 7L;
+        long characterCount = RepeatedString.repeatedString(repeatingString, characterLimit);
+
+        assertThat(characterCount, is(expectedCharacterCount));
+    }
+
+    @Test
+    public void repeatedString_problemDefinition_1() {
+
+        String repeatingString = "a";
+        long characterLimit = 1000000000000L;
+
+        long expectedCharacterCount  = 1000000000000L;
+        long characterCount = RepeatedString.repeatedString(repeatingString, characterLimit);
+
+        assertThat(characterCount, is(expectedCharacterCount));
+    }
+
+    @Test
+    public void repeatedString_problemDefinition_7() {
+
+        String repeatingString = "kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm";
+        long characterLimit = 736778906400L;
+
+        long expectedCharacterCount  = 51574523448L;
+        long characterCount = RepeatedString.repeatedString(repeatingString, characterLimit);
+
+        assertThat(characterCount, is(expectedCharacterCount));
+    }
+
+    @Test
+    public void repeatedString_oneCharacter_Matching() {
+
+        String repeatingString = "a";
+        long characterLimit = 1;
+
+        long expectedCharacterCount  = 1;
+        long characterCount = RepeatedString.repeatedString(repeatingString, characterLimit);
+
+        assertThat(characterCount, is(expectedCharacterCount));
+    }
+
+    @Test
+    public void repeatedString_oneCharacter_NotMatching() {
+
+        String repeatingString = "b";
+        long characterLimit = 1;
+
+        long expectedCharacterCount  = 0;
+        long characterCount = RepeatedString.repeatedString(repeatingString, characterLimit);
+
+        assertThat(characterCount, is(expectedCharacterCount));
+    }
+
+    @Test
+    public void repeatedString_manyCharacter_Matching_NoRepeating() {
+
+        String repeatingString = "aaabaaabaaa";
+        long characterLimit = 11;
+
+        long expectedCharacterCount  = 9;
+        long characterCount = RepeatedString.repeatedString(repeatingString, characterLimit);
+
+        assertThat(characterCount, is(expectedCharacterCount));
+    }
+
+    @Test
+    public void repeatedString_manyCharacter_Matching_RepeatingOnce() {
+
+        String repeatingString = "aaabaaabaaa";
+        long characterLimit = 22;
+
+        long expectedCharacterCount = 18;
+        long characterCount = RepeatedString.repeatedString(repeatingString, characterLimit);
+
+        assertThat(characterCount, is(expectedCharacterCount));
+    }
+
+}
