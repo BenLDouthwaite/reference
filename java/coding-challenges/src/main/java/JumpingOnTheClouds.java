77d9359871b5bf4c1bdb6719ea26ab9ec6a9f616
Ben Douthwaite <Ben.L.Douthwaite@gmail.com>
2020-02-05 10:52:05 +0900

    Adding Jumping on the clouds challenge and tests

---
+++ java/coding-challenges/src/main/java/JumpingOnTheClouds.java
@@ -0,0 +1,37 @@
+import java.util.Currency;
+
+public class JumpingOnTheClouds {
+
+    private static final int CUMULUS = 0;
+    private static final int THUNDERHEAD = 1;
+
+    // Complete the jumpingOnClouds function below.
+    public static int jumpingOnClouds(int[] c) {
+
+        int jumpCounter = 0;
+        for (int i = 0; i < c.length - 1; i++, jumpCounter++) {
+            if (i < c.length - 2 && c[i+2] == CUMULUS) {
+                i++; // Don't check jumped cloud
+            }
+        }
+        return jumpCounter;
+    }
+
+    // Complete the jumpingOnClouds function below.
+    public static int jumpingOnClouds_initialImplementation(int[] c) {
+
+        int jumpCounter = 0;
+        for (int i = 0; i < c.length - 1; i++) {
+            if (c[i+1] == THUNDERHEAD) {
+                i++; // Don't check jumped cloud
+            } else
+            if (i < c.length - 2
+                    && c[i+1] == CUMULUS
+                    && c[i+2] == CUMULUS) {
+                i++; // Don't check jumped cloud
+            }
+            jumpCounter++;
+        }
+        return jumpCounter;
+    }
+}
---
+++ java/coding-challenges/src/test/java/JumpingOnTheCloudsTest.java
@@ -0,0 +1,72 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class JumpingOnTheCloudsTest {
+
+    @Test
+    public void jumpingOnClouds_problemDefinition_0() {
+
+        int[] input = {0,0,1,0,0,1,0};
+        int expectedResult = 4;
+
+        int actualResult = JumpingOnTheClouds.jumpingOnClouds(input);
+        assertThat(actualResult, is(expectedResult));
+    }
+
+    @Test
+    public void jumpingOnClouds_problemDefinition_1() {
+
+        int[] input = {0,0,0,0,1,0};
+        int expectedResult = 3;
+
+        int actualResult = JumpingOnTheClouds.jumpingOnClouds(input);
+        assertThat(actualResult, is(expectedResult));
+    }
+
+    @Test
+    public void jumpingOnClouds_oneStepOneJump() {
+        int[] input = {0,0};
+        int expectedResult = 1;
+
+        int actualResult = JumpingOnTheClouds.jumpingOnClouds(input);
+        assertThat(actualResult, is(expectedResult));
+    }
+
+    @Test
+    public void jumpingOnClouds_twoStepsOneJump() {
+        int[] input = {0,0,0};
+        int expectedResult = 1;
+
+        int actualResult = JumpingOnTheClouds.jumpingOnClouds(input);
+        assertThat(actualResult, is(expectedResult));
+    }
+
+    @Test
+    public void jumpingOnClouds_threeStepsTwoJumps() {
+        int[] input = {0,0,0,0};
+        int expectedResult = 2;
+
+        int actualResult = JumpingOnTheClouds.jumpingOnClouds(input);
+        assertThat(actualResult, is(expectedResult));
+    }
+
+    @Test
+    public void jumpingOnClouds_CTC_1J() {
+        int[] input = {0,1,0};
+        int expectedResult = 1;
+
+        int actualResult = JumpingOnTheClouds.jumpingOnClouds(input);
+        assertThat(actualResult, is(expectedResult));
+    }
+
+    @Test
+    public void jumpingOnClouds_CTCTC_2J() {
+        int[] input = {0,1,0,1,0};
+        int expectedResult = 2;
+
+        int actualResult = JumpingOnTheClouds.jumpingOnClouds(input);
+        assertThat(actualResult, is(expectedResult));
+    }
+}
