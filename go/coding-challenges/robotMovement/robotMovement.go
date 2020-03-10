a33b64a31ac23dfe2cef34df2ddbf5233223fc5e
Ben L Douthwaite <Ben.L.Douthwaite@gmail.com>
2020-03-10 18:51:54 +0000

    Add conways game of life example in go and simplify robot movement challenge to avoid string cast

--- go/coding-challenges/robotMovement/robotMovement.go
+++ go/coding-challenges/robotMovement/robotMovement.go
@@ -8,11 +8,11 @@ func robotMovement(inputString string) int {
 	xStepCount := 0
 	yStepCount := 0
 	for _, c := range inputString {
-		switch string(c) {
-		case "N": yStepCount++
-		case "E": xStepCount++
-		case "S": yStepCount--
-		case "W": xStepCount--
+		switch c {
+		case 'N': yStepCount++
+		case 'E': xStepCount++
+		case 'S': yStepCount--
+		case 'W': xStepCount--
 		}
 	}
 	return int(math.Abs(float64(xStepCount)) + math.Abs(float64(yStepCount)))
