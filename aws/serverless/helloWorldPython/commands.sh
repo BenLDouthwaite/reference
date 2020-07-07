ca4946d7d0a4f7c84a794a468107cd348bd69dd1
Ben Douthwaite <BenjaminDouthwaite@rentalcars.com>
2020-07-07 22:55:20 +0100

    Gatling notes, serverless notes, lots of AWS stuff

---
+++ aws/serverless/helloWorldPython/commands.sh
@@ -0,0 +1,8 @@
+# Create an s3 bucket
+aws s3 mb s3://bld-code-sam
+
+# package cloudformation
+aws cloudformation package --s3-bucket bld-code-sam --template-file template.yaml --output-template-file gen/tempalte-generated.yaml
+
+# deploy
+aws cloudformation deploy --template-file /Users/DouthwaiteBe/dev/personal/reference/aws/serverless/helloWorldPython/gen/tempalte-generated.yaml --stack-name hello-world-sam --capabilities CAPABILITY_IAM
