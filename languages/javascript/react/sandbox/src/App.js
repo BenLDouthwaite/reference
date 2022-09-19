27ea06a2544b03a67c2584a16a38d9a0771e256b
Ben Douthwaite <Ben.L.Douthwaite@gmail.com>
2022-09-19 19:51:26 +0100

    Extracting seasons project as a page in the sandbox project

--- languages/javascript/react/sandbox/src/App.js
+++ languages/javascript/react/sandbox/src/App.js
@@ -2,52 +2,20 @@ import React from "react";
 
 import './App.css';
 
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Route, Routes } from "react-router";
+
+import Seasons from "./pages/seasons";
+import Home from "./pages/home";
 
 function App() {
   return (
     <BrowserRouter>
-      <Link to="/" >
-        Home
-      </Link>
-      <Link to="/about" >
-        About
-      </Link>
-      <Link
-        to="/dashboard">
-        Dashboard
-      </Link>
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/dashboard" element={<Dashboard />}>
-      </Route>
-    </Routes>  </BrowserRouter>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
+        <Route path="/" element={<Home />} />
+        <Route path="/seasons" element={<Seasons />} />
+      </Routes>  
+    </BrowserRouter>
   );
 }
 
---
+++ languages/javascript/react/sandbox/src/pages/home/index.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+    return (
+      <div>
+        <h2>React Sandbox</h2>
+        <ul>
+          <li>
+          <Link to="/" >
+          Home
+        </Link>
+          </li>
+          <li>
+        
+          <Link
+          to="/seasons">
+          Seasons
+        </Link>
+          </li>
+        </ul>
+
+      </div>
+    );
+  }
