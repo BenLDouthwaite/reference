e4ac102686f784ec8fbfe9ea029401701a4ac424
bld <bld.code@pm.me>
2023-09-21 23:34:23 +0100

    AOC-2020, day 3, Rust naive implementation

--- rust/aoc-2020/src/bin/day3.rs
+++ rust/aoc-2020/src/bin/day3.rs
@@ -1,19 +1,42 @@
 fn day3_p1(input: &str) -> u32 {
-    let char_vec_vec: Vec<_> = input.lines()
-        .map(|line| line.chars().collect::<Vec<_>>())
-        .collect();
-    let char_vec_vec = dbg!(char_vec_vec);
-    println!("{}", char_vec_vec[0][3]);
-    println!("{}", char_vec_vec[0][4]);
-
-    // todo loop for number of rows in input
-    // with each iteration, increase coordinates by 3 right, down 1
-    return 1
+    let grid = parse_grid(input);
+    return count_trees(&grid, 3, 1);
 }
 fn day3_p2(input: &str) -> u32 {
-    return 1
-}
+    let grid = parse_grid(input);
+    let a = count_trees(&grid, 1, 1);
+    let b = count_trees(&grid, 3, 1);
+    let c = count_trees(&grid, 5, 1);
+    let d = count_trees(&grid, 7, 1);
+    let e = count_trees(&grid, 1, 2);
 
+    return a * b * c * d * e;
+}
+fn parse_grid(input: &str) -> Vec<Vec<char>> {
+    let grid: Vec<_> = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect();
+    return grid;
+}
+fn count_trees(grid: &Vec<Vec<char>>, dx: usize, dy: usize) -> u32 {
+    let mut x = 0;
+    let mut y = 0;
+    let lines = grid.len();
+    let line_len = grid[0].len();
+    let mut tree_counter = 0;
+
+    while y + dy < lines {
+        y = y + dy;
+        x = (x + dx) % line_len;
+        let value = grid[y][x];
+        if value == '#' {
+            tree_counter = tree_counter + 1;
+        }
+    }
+
+    return tree_counter;
+}
 #[cfg(test)]
 mod tests {
 
@@ -27,28 +50,23 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn day3_p1_test() {
         let input = include_str!("../../inputs/day3.txt");
         let result = day3_p1(input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 203);
     }
 
     #[test]
-    #[ignore]
     fn day3_p2_example_test() {
         let input = include_str!("../../inputs/day3_example.txt");
         let result = day3_p2(input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 336);
     }
 
     #[test]
-    #[ignore]
     fn day3_p2_test() {
         let input = include_str!("../../inputs/day3.txt");
         let result = day3_p2(input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 3316272960);
     }
-
-
 }
