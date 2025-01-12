f28c41ea928bc3dd68b7c96d64f6d30f726bb1b9
bld <bld.code@pm.me>
2025-01-12 19:42:38 +0000

    LC: Binary Trees, Python@

---
+++ python/leetcode/test_p199_binary_tree_right_side.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestSolution(TestCase):
+    def test_right_side_view(self):
+        self.fail()
---
+++ python/leetcode/test_p236_lowest_common_ancestor_binary_tree.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestSolution(TestCase):
+    def test_lowest_common_ancestor(self):
+        self.fail()
