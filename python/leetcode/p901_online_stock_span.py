d82133d82b54eb9d96913857235358672c2675f1
bld <bld.code@pm.me>
2025-01-22 19:57:05 +0000

    LC: PY: Monotonic stack

--- python/leetcode/p901_online_stock_span.py
+++ python/leetcode/p901_online_stock_span.py
@@ -5,10 +5,34 @@ class StockSpanner:
 
     def __init__(self):
         self.stack = deque()
+        self.day = 0
 
     def next(self, price: int) -> int:
-        return 0
+        self.day += 1
+
+        while self.stack and self.stack[-1][0] <= price:
+            self.stack.pop()
+
+        if not self.stack:
+            self.stack.append((price, self.day))
+            return self.day
+
+        last_day_higher_than_today = self.stack[-1][1]
+        self.stack.append((price, self.day))
+        return self.day - last_day_higher_than_today
 
 # Your StockSpanner object will be instantiated and called as such:
 # obj = StockSpanner()
-# param_1 = obj.next(price)
+# res = []
+# for v in [100, 80, 60, 70, 60, 75, 85]:
+#     val = obj.next(v)
+#     res.append(val)
+# print(res)
+
+
+# obj = StockSpanner()
+# res = []
+# for v in [31,41,48,59,79]:
+#     val = obj.next(v)
+#     res.append(val)
+# print(res)
